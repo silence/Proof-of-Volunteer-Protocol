ea718f3b9cc799da49c898b7aec7c13012d3c669
Web3 - Help & Grow <jlzxwt8@126.com>
2023-03-07 14:41:42 +0800

    Update attendees.ts

--- src/json/attendees.ts
+++ src/json/attendees.ts
@@ -3,28 +3,31 @@ import { Attendee } from '@/types/attendee';
 export const ATTENDEES: Attendee[] = [
   {
     name: 'Tony',
-    role: 'SeeDAO 成员',
+    role: '城市联络人',
     email: 'tongwang.zh@outlook.com',
+    introduction: 'SeeDAO新加坡城市联络人',
     wallet_address: '0x4B162DBa01943Bd8CD8af668bE72fB769999aC83'
   },
   {
     name: 'Mia',
-    role: '城市联络人',
-    email: 'herminazhang@gmail.com'
+    role: '新人营',
+    email: 'herminazhang@gmail.com',
+    introduction: 'Help & Grow社群发起人之一',
+    wallet_address: '0x9f0a97311761d0560ae9496ec76f6b58c7970e3e'
   },
   {
     name: 'Siyi',
-    role: '翻译公会',
+    role: 'SeeDAO 成员',
     email: 'iris921426135@gmail.com'
   },
   {
     name: 'Xueying',
-    role: '投研公会',
+    role: 'SeeDAO 成员',
     email: 'xueey.wang@gmail.com'
   },
   {
     name: 'Darren',
     role: 'SeeDAO 成员',
-    email: 'darren@outlook.com'
+    email: 'ldz092@gmail.com'
   }
 ];
