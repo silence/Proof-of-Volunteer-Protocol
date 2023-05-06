67701fb88f1c33b27cf5effa66011ae785274640
luchenghao <luchenghao@luchenghaos-MacBook-Pro.local>
2023-05-06 23:28:24 +0800

    Delete eslint prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,17 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'prettier'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        tabWidth: 2,
-        printWidth: 100,
-        singleAttributePerLine: false,
-        singleQuote: true,
-        semi: true,
-        trailingComma: 'none' // Will be the default in Prettier 3.0
-      }
-    ],
-    'linebreak-style': ['error', 'unix'],
-    "endOfLine": "auto"
-  }
+  extends: ['next/core-web-vitals'],
+  // rules: {
+  //   'prettier/prettier': [
+  //     'error',
+  //     {
+  //       tabWidth: 2,
+  //       printWidth: 100,
+  //       singleAttributePerLine: false,
+  //       singleQuote: true,
+  //       semi: true,
+  //       trailingComma: 'none' // Will be the default in Prettier 3.0
+  //     }
+  //   ],
+  //   'linebreak-style': ['error', 'unix'],
+  // }
 };
