cffb7c70f3dbbc1cc1ca89991f62c2d857682460
zezhang.liu <zezhang.liu@shopee.com>
2023-08-14 23:13:13 +0800

    feat(povp): remove useless code in connect-wallet

--- src/pages/povp/connect-wallet/index.tsx
+++ src/pages/povp/connect-wallet/index.tsx
@@ -1,22 +1,10 @@
 import React, { useEffect, useState } from "react";
-import {
-  Card,
-  Result,
-  Button,
-  Space,
-  Toast,
-  Dialog,
-  Form,
-  Input,
-  NoticeBar,
-} from "antd-mobile";
+import { Card, Result, Button, Dialog } from "antd-mobile";
 import { SmileOutline } from "antd-mobile-icons";
 import styles from "@/styles/common.module.css";
-import { Web3Button, Web3NetworkSwitch } from "@web3modal/react";
+import { Web3NetworkSwitch } from "@web3modal/react";
 import { useAccount, useContractWrite, usePrepareContractWrite } from "wagmi";
-import { convertBase64, postData } from "@/util";
 import abiJson from "@/povp_abi.json";
-import { CONTRACT_ADDRESS } from "@/constants";
 import { useRouter } from "next/router";
 import { useGlobalState } from "@/hooks/globalContext";
 import { povp_Contract_Address, Web3StorageApi } from "@/constants";
@@ -25,49 +13,10 @@ import Web3 from "web3";
 import Image from "next/image";
 export interface ConnectWalletPageProps {}
 
-// const useUploadImage = ({
-//   imageObj,
-//   isConnected
-// }: {
-//   imageObj: ImageObjType;
-//   isConnected: boolean;
-// }) => {
-//   const [id, setId] = useState('init');
-
-//   useEffect(() => {
-//     (async function extractFile() {
-//       try {
-//         if (isConnected && imageObj.name.length > 0 && imageObj.bucket.length > 0) {
-//           // const file = await (await fetch(blobUrl)).blob();
-//           // const fileBase64 = await convertBase64(file);
-//           // const formData = new FormData();
-//           // formData.append('imageFile', file, file.name);
-//           // const res = await postData('/api/upload-image', {
-//           //   content: fileBase64,
-//           //   fileName
-//           // });
-//           setId(`https://${imageObj.bucket}.4everland.store/${imageObj.name}`);
-//         }
-//       } catch (e: any) {
-//         console.log('e', e);
-//         // Dialog.alert({
-//         //   content: e?.message || 'Error',
-//         //   confirmText: 'Dismiss'
-//         // });
-//         setId('debug');
-//       }
-//     })();
-//   }, [isConnected, imageObj]);
-
-//   return [id];
-// };
-
-const ConnectWalletPage: React.FC<ConnectWalletPageProps> = (props) => {
-  const [imageUrl, setImageUrl] = useState<string>("");
+const ConnectWalletPage: React.FC<ConnectWalletPageProps> = () => {
   const { isConnected } = useAccount();
   const router = useRouter();
-  const { recipient, ipfsImageUrl, email } = useGlobalState();
-  // const [imageId] = useUploadImage({ imageObj: imageUrl, isConnected });
+  const { ipfsImageUrl, email } = useGlobalState();
   const [walletAddress, setWalletAddress] = useState<string>();
   const [metaData, setmetaData] = useState<string>();
 
@@ -83,9 +32,7 @@ const ConnectWalletPage: React.FC<ConnectWalletPageProps> = (props) => {
     functionName: "mint",
     args: [walletAddress, metaData],
   });
-  const { data, isLoading, isSuccess, write } = useContractWrite(config);
-
-  // console.log('data', data, isSuccess, walletAddress, imageUrl);
+  const { isLoading, isSuccess, write } = useContractWrite(config);
 
   const handleMint = async () => {
     var web3 = new Web3(Web3.givenProvider);
@@ -140,13 +87,6 @@ const ConnectWalletPage: React.FC<ConnectWalletPageProps> = (props) => {
     }
   };
 
-  // useEffect(() => {
-  //   const fileName = localStorage.getItem('ImageFileName') ?? '';
-  //   const bucketName = localStorage.getItem('BucketName') ?? '';
-  //   if (fileName.length && bucketName.length)
-  //     setImageUrl(`https://${bucketName}.4everland.store/${fileName}`);
-  // }, []);
-
   useEffect(() => {
     if (isSuccess) {
       const res = Dialog.alert({
@@ -159,12 +99,6 @@ const ConnectWalletPage: React.FC<ConnectWalletPageProps> = (props) => {
     }
   }, [isSuccess, router]);
 
-  // useEffect(() => {
-  //   if (recipient) {
-  //     setWalletAddress(recipient.wallet_address);
-  //   }
-  // }, [recipient]);
-
   return (
     <div className={styles.app}>
       <div className={styles.body}>
@@ -175,44 +109,9 @@ const ConnectWalletPage: React.FC<ConnectWalletPageProps> = (props) => {
             title="Connect Wallet!"
           />
 
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              marginBottom: "24px",
-            }}
-          >
-            {imageUrl.length && isConnected && (
-              <Image
-                src={imageUrl}
-                alt=""
-                style={{ maxHeight: "300px", width: "auto", maxWidth: "100%" }}
-              />
-            )}
-          </div>
-
           <div style={{ textAlign: "center", margin: "32px 0px" }}>
             <Web3NetworkSwitch />
-            {/* <Web3Button icon="show" label="Connect Wallet" balance="show"></Web3Button> */}
           </div>
-
-          {/* {isConnected && (
-            <Form
-              onValuesChange={({ wallet_address }) => {
-                setWalletAddress(wallet_address);
-              }}
-            >
-              <NoticeBar
-                content={`As ${recipient?.name} did not bind Etherum address or Unipass account, please input the target address`}
-                wrap
-                color="alert"
-              />
-
-              <Form.Item name="wallet_address">
-                <Input placeholder="Input wallet address"></Input>
-              </Form.Item>
-            </Form>
-          )} */}
 
           {isConnected && (
             <Button
